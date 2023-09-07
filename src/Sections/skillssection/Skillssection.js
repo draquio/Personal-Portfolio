47ca4449440a40b5612efee153ec7f5efc9f4407
draquio <draquio@gmail.com>
2023-09-07 11:15:17 -0400

    Agregando Otras herramientas

--- src/Sections/skillssection/Skillssection.js
+++ src/Sections/skillssection/Skillssection.js
@@ -13,59 +13,150 @@ export function Skillssection() {
           <h4>FrontEnd</h4>
           <div className="tecnology_content">
             <div className="icon_container">
-              <Image alt="tecnología frontend HTML" className="icon_skill" src="/tecnologies/html.png" />
+              <Image
+                alt="tecnología frontend HTML"
+                className="icon_skill"
+                src="/tecnologies/html.png"
+              />
               HTML
             </div>
             <div className="icon_container">
-              <Image alt="tecnología frontend CSS" className="icon_skill" src="/tecnologies/css.png" />
+              <Image
+                alt="tecnología frontend CSS"
+                className="icon_skill"
+                src="/tecnologies/css.png"
+              />
               CSS
             </div>
             <div className="icon_container">
-              <Image alt="tecnología frontend Javascript" className="icon_skill" src="/tecnologies/javascript.png" />
+              <Image
+                alt="tecnología frontend Javascript"
+                className="icon_skill"
+                src="/tecnologies/javascript.png"
+              />
               JavaScript
             </div>
 
             <div className="icon_container">
-              <Image alt="tecnología frontend React" className="icon_skill" src="/tecnologies/react.png" />
+              <Image
+                alt="tecnología frontend React"
+                className="icon_skill"
+                src="/tecnologies/react.png"
+              />
               React
             </div>
             <div className="icon_container">
-              <Image alt="tecnología frontend Bootstrap" className="icon_skill" src="/tecnologies/bootstrap.png" />
+              <Image
+                alt="tecnología frontend Bootstrap"
+                className="icon_skill"
+                src="/tecnologies/bootstrap.png"
+              />
               Bootstrap
             </div>
             <div className="icon_container">
-              <Image alt="tecnología frontend Sass" className="icon_skill" src="/tecnologies/sass.png" />
+              <Image
+                alt="tecnología frontend Sass"
+                className="icon_skill"
+                src="/tecnologies/sass.png"
+              />
               Sass
             </div>
           </div>
           <h4>Backend</h4>
           <div className="tecnology_content">
             <div className="icon_container">
-              <Image alt="tecnología backend PHP" className="icon_skill" src="/tecnologies/php.png" />
+              <Image
+                alt="tecnología backend PHP"
+                className="icon_skill"
+                src="/tecnologies/php.png"
+              />
               PHP
             </div>
             <div className="icon_container">
-              <Image alt="tecnología backend NodeJS" className="icon_skill" src="/tecnologies/node-js.png" />
+              <Image
+                alt="tecnología backend NodeJS"
+                className="icon_skill"
+                src="/tecnologies/node-js.png"
+              />
               NodeJS
             </div>
             <div className="icon_container">
-              <Image alt="tecnología backend Python" className="icon_skill" src="/tecnologies/python.png" />
+              <Image
+                alt="tecnología backend Python"
+                className="icon_skill"
+                src="/tecnologies/python.png"
+              />
               Python
             </div>
           </div>
-          <h4>DataBase</h4>
+          <h4>Base de datos</h4>
           <div className="tecnology_content">
             <div className="icon_container">
-              <Image alt="tecnología base de datos MongoDB" className="icon_skill" src="/tecnologies/mongodb.png" />
+              <Image
+                alt="tecnología base de datos MongoDB"
+                className="icon_skill"
+                src="/tecnologies/mongodb.png"
+              />
               MongoDB
             </div>
             <div className="icon_container">
-              <Image alt="tecnología base de datos MySQL" className="icon_skill" src="/tecnologies/mysql.png" />
+              <Image
+                alt="tecnología base de datos MySQL"
+                className="icon_skill"
+                src="/tecnologies/mysql.png"
+              />
               MySQL
             </div>
             <div className="icon_container">
-              <Image alt="tecnología base de datos SQL Server" className="icon_skill" src="/tecnologies/sql-server.png" />
+              <Image
+                alt="tecnología base de datos SQL Server"
+                className="icon_skill"
+                src="/tecnologies/sql-server.png"
+              />
               SQLServer
+            </div>
+          </div>
+          <h4>Otras Herramientas</h4>
+          <div className="tecnology_content">
+            <div className="icon_container">
+              <Image
+                alt="tecnología Git"
+                className="icon_skill"
+                src="/tecnologies/git.png"
+              />
+              Git
+            </div>
+            <div className="icon_container">
+              <Image
+                alt="Seo"
+                className="icon_skill"
+                src="/tecnologies/SEO.png"
+              />
+              Seo
+            </div>
+            <div className="icon_container">
+              <Image
+                alt="tecnología wordpress"
+                className="icon_skill"
+                src="/tecnologies/wordpress.png"
+              />
+              Wordpress
+            </div>
+            <div className="icon_container">
+              <Image
+                alt="tecnología Photoshop"
+                className="icon_skill"
+                src="/tecnologies/Photoshop.png"
+              />
+              Photoshop
+            </div>
+            <div className="icon_container">
+              <Image
+                alt="Inglés B2"
+                className="icon_skill"
+                src="/tecnologies/english.png"
+              />
+              Inglés B2
             </div>
           </div>
         </div>
