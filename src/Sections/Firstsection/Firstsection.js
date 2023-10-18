427c4b9c1e5e3eccdbfc6bdf61ff7f5eb26ab433
draquio <draquio@gmail.com>
2023-10-18 11:02:17 -0400

    probando boton de descarga

--- src/Sections/Firstsection/Firstsection.js
+++ src/Sections/Firstsection/Firstsection.js
@@ -3,14 +3,17 @@ import Typed from "typed.js";
 import "./firstsection.scss";
 import { Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import 'semantic-ui-css/components/icon.min.css';
+import "semantic-ui-css/components/icon.min.css";
 
 export function Firstsection(props) {
   const { language } = props;
   const el = useRef(null);
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: [`&lt;${language.FS.rol.dev}/&gt;`, `&lt;${language.FS.rol.ing}/&gt;`],
+      strings: [
+        `&lt;${language.FS.rol.dev}/&gt;`,
+        `&lt;${language.FS.rol.ing}/&gt;`,
+      ],
       typeSpeed: 100,
       backSpeed: 50,
       startDelay: 200,
@@ -30,26 +33,41 @@ export function Firstsection(props) {
         <div className="dev_title">
           <span ref={el} className="dev_title_span"></span>
         </div>
-        <span className="summary_about_me" dangerouslySetInnerHTML={{__html:language.FS.content}}>
-        </span>
+        <span
+          className="summary_about_me"
+          dangerouslySetInnerHTML={{ __html: language.FS.content }}
+        ></span>
         <div className="icons_profile">
-          <Link target="_blank" to='https://www.linkedin.com/in/sergio-mercado-galarza-4a4b42274/'>
+          <Link
+            target="_blank"
+            to="https://www.linkedin.com/in/sergio-mercado-galarza-4a4b42274/"
+          >
             <Icon size="large" name="linkedin" />
           </Link>
-          <Link target="_blank" to='https://www.facebook.com/sergio.mercado.16568/'>
-          <Icon size="large" name="facebook" />
+          <Link
+            target="_blank"
+            to="https://www.facebook.com/sergio.mercado.16568/"
+          >
+            <Icon size="large" name="facebook" />
           </Link>
-          <Link target="_blank" to='https://www.instagram.com/draquio/'>
-          <Icon size="large" name="instagram" />
+          <Link target="_blank" to="https://www.instagram.com/draquio/">
+            <Icon size="large" name="instagram" />
           </Link>
-          <Link target="_blank" to='https://www.youtube.com/channel/UCq1GwPQcE1fu8uE8up59JVQ'>
-          <Icon size="large" name="youtube" />
+          <Link
+            target="_blank"
+            to="https://www.youtube.com/channel/UCq1GwPQcE1fu8uE8up59JVQ"
+          >
+            <Icon size="large" name="youtube" />
           </Link>
-          
-          
         </div>
         <div className="download_button">
-          <a className="btn btn_yellow" download="Ing_Sergio_Mercado_CV.pdf" href={`${process.env.PUBLIC_URL}/document/Ing_Sergio_Mercado_CV.pdf`} download>{language.FS.button} <Icon  name="download" /></a>
+          <a
+            className="btn btn_yellow"
+            download
+            href={`/document/Ing_Sergio_Mercado_CV.pdf`}
+          >
+            {language.FS.button} <Icon name="download" />
+          </a>
         </div>
       </div>
     </div>
