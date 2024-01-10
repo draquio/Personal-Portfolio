fe7df95a71849331200b4d2210ffc3ceca784037
draquio <draquio@gmail.com>
2024-01-10 18:54:51 -0400

    quitando mi api

--- src/Sections/Contactsection/Contactsection.js
+++ src/Sections/Contactsection/Contactsection.js
@@ -18,7 +18,7 @@ export function Contactsection(props) {
   const sendContactForm = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    emailjs.init("w_AaTYYpc9CxEFaVP");
+    emailjs.init("");
     let templateParams = {
       user_name: e.target.email.value,
       user_email: e.target.user.value,
