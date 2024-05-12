1ece98a5611099234f198ae00c26806c5918a7c0
draquio <draquio@gmail.com>
2024-05-12 15:07:28 -0400

    actualizando CV en español 12/05/2024

---
+++ src/Sections/Statssection/Statssection.js
@@ -0,0 +1,20 @@
+import "./Statssection.scss"
+
+export const Statssection = () => {
+  return (
+    <section style={{minHeight:'600px'}}>
+      <div className="stats_section">
+        <div className="stats_section_item">
+            <h2>+14</h2>
+            <p>PROYECTOS</p>
+        </div>
+        <div className="stats_section_item">
+            <h2>+1.5</h2>
+            <p>AÑOS</p>
+        </div>
+        <div className="stats_section_item"></div>
+      </div>
+    </section>
+  )
+}
+
